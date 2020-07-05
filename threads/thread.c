bae8eaf38f3b7fac28e91976bf1bd3b0daa97dd1
dfbsdan <dfbsdaniel11@hotmail.com>
2020-07-05 14:02:54 +0900

    Testing

--- threads/thread.c
+++ threads/thread.c
@@ -251,9 +251,11 @@ thread_create (const char *name, int priority,
 		return TID_ERROR;
 	}
 	if (curr->curr_dir) {
+		printf("thread_create: opening dir for: %s\n", name);///////////////////////TESTING LINE
 		t->curr_dir = dir_open (dir_get_inode (curr->curr_dir));
 	} else {
 		ASSERT (curr == initial_thread);
+		printf("thread_create: opening root for: %s\n", name);//////////////////////TESTING LINE
 		t->curr_dir = (function != idle)?: dir_open_root (); NULL;
 	}
 	if (!t->curr_dir && function != idle)
